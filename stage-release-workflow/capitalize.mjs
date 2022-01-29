fbf794f0db2f9ebf5b0058109ed10ecf1d6078ba
pelaakso <pelaakso@users.noreply.github.com>
2022-01-29 16:30:01 +0200

    Capitalizing already capitalized has no effect

--- stage-release-workflow/capitalize.mjs
+++ stage-release-workflow/capitalize.mjs
@@ -2,3 +2,6 @@ import caps from 'just-capitalize';
 
 const hello = 'hello';
 console.log(`${hello} as capitalized: ${caps(hello)}`);
+
+const hello2 = 'Hello';
+console.log(`${hello2} as capitalized: ${caps(hello2)}`);
